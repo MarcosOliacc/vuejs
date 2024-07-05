32c633d14fd3079be253185ff38070c04fd81a3a
Marcos Vinicius <marcosoliacc@gmail.com>
2024-07-05 20:59:25 -0300

    who is atualização

--- 03-treinandoConteudos/02-pokemons/src/components/WhoIsSect/WhoIsSect.vue
+++ 03-treinandoConteudos/02-pokemons/src/components/WhoIsSect/WhoIsSect.vue
@@ -4,23 +4,37 @@ import { onMounted, reactive, ref } from 'vue';
 const pokedata = reactive({pokemon: {}})
 const divActived = ref('form')
 const response = ref('')
-const number = parseInt(Math.random()* 1025).toString().padStart(3,'0')
+const loading = ref(false)
 
 async function fetchOnePokemon() {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
-    const data = await res.json()
-    pokedata.pokemon = data
-    console.log(pokedata.pokemon.id)
+    loading.value = true
+    const number = parseInt(Math.random()* 1025).toString().padStart(3,'0')
+    
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+        const data = await res.json();
+        pokedata.pokemon = data;
+    } catch (error) {
+        console.error('Erro ao buscar Pokémon:', error);
+    } finally {
+        loading.value = false;
+    }
 }
 
 function tryAgain() {
     divActived.value = 'form'
     response.value = ''
 }
+function again() {
+    fetchOnePokemon()
+
+    divActived.value = 'form'
+    response.value = ''
+}
 async function handleSubmit(ev) {
     ev.preventDefault()
-    if (response.value != '') {
-        if(response.value == pokedata.pokemon.name) {
+    if (response.value.trim() != '') {
+        if(response.value.toLocaleLowerCase() === pokedata.pokemon.name.toLocaleLowerCase) {
             alert('parabens! você acertou.')
             divActived.value = 'trueDiv'
         } else {
@@ -37,7 +51,24 @@ onMounted(fetchOnePokemon)
 <template>
     <section class="conteiner">
         <div class="podiumContent">
-            <div class="off">
+            <div v-if="divActived == 'form'&& !loading" class="off">
+                <div class="shadowBoxX">
+                    <div class="shadowBoxY">
+                        <img class="pokeImg" v-bind:src="'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/'+pokedata.pokemon.id+'.png'" alt="">
+                    </div>
+                </div>
+            </div>
+            <div v-else-if="loading">
+                <div class="off">
+                <div class="shadowBoxX">
+                    <div class="shadowBoxY">
+                        <p>carregando...</p>
+                    </div>
+                </div>
+            </div>
+                
+            </div>
+            <div v-else class="on">
                 <div class="shadowBoxX">
                     <div class="shadowBoxY">
                         <img class="pokeImg" v-bind:src="'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/'+pokedata.pokemon.id+'.png'" alt="">
@@ -57,6 +88,7 @@ onMounted(fetchOnePokemon)
         </div>
         <div v-else-if="divActived == 'trueDiv'" class="falseResConteiner">
             <h1>acertou</h1>
+            <button @click="again">Tente novamente!</button>
         </div>
         <div v-else-if="divActived == 'falseDiv'" class="defConteiner">
             <h3>Que pena, parece que você errou! </h3>
